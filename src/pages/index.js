365519081b8d5a509b4ccfce9f2eaa21e9f850d3
MagicRomero <magicromero@protonmail.com>
2021-03-07 21:11:09 +0000

    Setup nginx as reverse proxy for next dockerized project

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to my world<a href="https://nextjs.org">Next.js!</a>
+          Welcome to dddddmy world<a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
